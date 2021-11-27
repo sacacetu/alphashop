ee9e04788b3bc166fb81fe2b0a06c18d031fd734
Samuele <s_casula@hotmail.com>
2021-11-27 17:41:54 +0100

    Sezione 4

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { SalutiDataService } from '../services/data/saluti-data.service';
 
 @Component({
   selector: 'app-welcome',
@@ -10,10 +11,33 @@ export class WelcomeComponent implements OnInit {
   saluti: string = 'Benvenuti nel sito Alphashop';
   titolo2 = 'seleziona gli articoli da acquistare';
   utente = '';
+  messaggio = '';
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private salutiSrv: SalutiDataService
+  ) {}
 
   ngOnInit(): void {
     this.utente = this.route.snapshot.params['userId'];
+  }
+
+  getSaluti() {
+    this.salutiSrv
+      .getSaluti(this.utente)
+      .subscribe(
+        response => this.handleResponse(response),
+        error => this.handleError(error)
+        );
+  }
+
+  handleResponse(response: any) {
+    this.messaggio = response.toString();
+    console.log(response);
+  }
+
+  handleError(error: any){
+    this.messaggio = error.error.text;
+    console.log(error);
   }
 }
